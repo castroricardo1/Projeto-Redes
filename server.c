03f7b87013cd94b815fc2eaad1bd7f89ea3cd302
Ricardo Loureiro <ricardoloureiro75@gmail.com>
2023-06-26 19:09:11 -0300

    modificando para System call select()
    Co-authored-by: Ana Carolina Rodrigues <anacarolinarodrigues480@gmail.com>

--- server.c
+++ server.c
@@ -3,8 +3,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <arpa/inet.h>
-#include <pthread.h>
+#include <errno.h>
 
 #define MAX_CLIENTS 10
 #define MAX_ROOMS 5
@@ -26,6 +27,8 @@ typedef struct {
 
 Room* rooms[MAX_ROOMS];
 int num_rooms = 0;
+int max_sd; // Valor máximo do descritor de arquivo (socket) para a chamada select()
+fd_set read_fds; // Conjunto de descritores de arquivo para leitura
 
 void send_message(int socket, const char* message) {
     write(socket, message, strlen(message));
@@ -138,9 +141,23 @@ void handle_client_message(Client* client, const char* message) {
     }
 }
 
+void handle_new_connection(int server_socket) {
+    int client_socket;
+    struct sockaddr_in client_address;
+    socklen_t client_address_len = sizeof(client_address);
 
-void* handle_client(void* arg) {
-    Client* client = (Client*)arg;
+    client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_len);
+    if (client_socket == -1) {
+        perror("Erro ao aceitar a conexão");
+        return;
+    }
+
+    printf("Cliente conectado\n");
+
+    Client* client = (Client*)malloc(sizeof(Client));
+    client->id = client_socket;
+    memset(client->name, 0, MAX_NAME_LENGTH);
+    client->socket = client_socket;
 
     send_message(client->socket, "Insira seu nome:\n");
     read(client->socket, client->name, MAX_NAME_LENGTH);
@@ -166,7 +183,7 @@ void* handle_client(void* arg) {
             send_message(client->socket, "A sala está cheia.\n");
             close(client->socket);
             free(client);
-            return NULL;
+            return;
         }
     } else if (room_id == -1) {
         send_message(client->socket, "Insira o limite da sala:\n");
@@ -185,32 +202,24 @@ void* handle_client(void* arg) {
             send_message(client->socket, "Não foi possível criar a sala.\n");
             close(client->socket);
             free(client);
-            return NULL;
+            return;
         }
     } else {
         send_message(client->socket, "Sala não encontrada.\n");
         close(client->socket);
         free(client);
-        return NULL;
+        return;
     }
 
-    while (1) {
-        char message[1000];
-        int bytes_read = read(client->socket, message, sizeof(message));
-        if (bytes_read <= 0) {
-            printf("Cliente desconectado\n");
-            break;
-        }
-        message[bytes_read] = '\0';
+    // Adiciona o descritor de arquivo do novo cliente ao conjunto
+    FD_SET(client_socket, &read_fds);
 
-        handle_client_message(client, message);
-    }
+    // Atualiza o valor máximo do descritor de arquivo
+    if (client_socket > max_sd)
+        max_sd = client_socket;
 
-    close(client->socket);
-    free(client);
-    return NULL;
+    printf("Cliente adicionado à sala\n");
 }
-
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
@@ -218,9 +227,11 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int server_socket, client_socket;
-    struct sockaddr_in server_address, client_address;
-    socklen_t client_address_len = sizeof(client_address);
+    int server_socket, client_socket, max_clients = 0;
+    int activity, i, valread, sd;
+    struct sockaddr_in server_address;
+    socklen_t client_address_len = sizeof(server_address);
+    char buffer[1000];
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
@@ -244,26 +255,57 @@ int main(int argc, char* argv[]) {
 
     printf("Servidor de bate-papo iniciado. Aguardando conexões...\n");
 
+    FD_ZERO(&read_fds); // Limpa o conjunto de descritores de arquivo para leitura
+    FD_SET(server_socket, &read_fds); // Adiciona o socket do servidor ao conjunto
+    max_sd = server_socket; // Valor máximo do descritor de arquivo (socket)
+
     while (1) {
-        client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_address_len);
-        if (client_socket == -1) {
-            perror("Erro ao aceitar a conexão");
-            return 1;
+        fd_set temp_fds = read_fds; // Copia o conjunto de descritores de arquivo para leitura
+
+        // Aguarda atividade em algum descritor de arquivo usando a chamada select()
+        activity = select(max_sd + 1, &temp_fds, NULL, NULL, NULL);
+        if ((activity < 0) && (errno != EINTR)) {
+            printf("Erro ao aguardar por atividade de descritor de arquivo\n");
         }
 
-        printf("Cliente conectado\n");
-
-        Client* client = (Client*)malloc(sizeof(Client));
-        client->id = client_socket;
-        memset(client->name, 0, MAX_NAME_LENGTH);
-        client->socket = client_socket;
-
-        pthread_t tid;
-        if (pthread_create(&tid, NULL, handle_client, (void*)client) != 0) {
-            perror("Erro ao criar a thread");
-            return 1;
+        // Novo cliente se conectou
+        if (FD_ISSET(server_socket, &temp_fds)) {
+            handle_new_connection(server_socket);
         }
-        pthread_detach(tid);
+
+        // Verifica por atividade nos descritores de arquivo dos clientes
+        for (i = 0; i < num_rooms; i++) {
+            Room* room = rooms[i];
+            for (int j = 0; j < room->num_clients; j++) {
+                sd = room->clients[j]->socket;
+
+                if (FD_ISSET(sd, &temp_fds)) {
+                    valread = read(sd, buffer, sizeof(buffer));
+                    if (valread == 0) {
+                        // Cliente desconectou
+                        struct sockaddr_in client_address;
+                        socklen_t client_address_len = sizeof(client_address);
+                        getpeername(sd, (struct sockaddr*)&client_address, &client_address_len);
+                        printf("Cliente desconectado, endereço IP: %s, porta: %d\n",
+                            inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
+
+                        // Fecha o descritor de arquivo e remove o cliente da sala
+                        close(sd);
+                        remove_client_from_room(room->clients[j], room);
+                        free(room->clients[j]);
+
+                        // Remove o descritor de arquivo do conjunto
+                        FD_CLR(sd, &read_fds);
+
+                        printf("Cliente removido da sala\n");
+                    } else {
+                        // Trata a mensagem do cliente
+                        buffer[valread] = '\0';
+                        handle_client_message(room->clients[j], buffer);
+                    }
+                }
+            }
+        }
     }
 
     close(server_socket);
